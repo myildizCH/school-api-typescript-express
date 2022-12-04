21b45746c931d094e205eeb1b59266fc7c7b40f6
myildizCH <meyildiz.de@gmail.com>
2022-12-04 23:33:37 +0100

    adds routes

--- app.ts
+++ app.ts
@@ -4,7 +4,14 @@ const app = express();
 app.use(express.json());
 
 import indexRouter from "./index";
+import classroomRouter from "./src/classroom/route";
+import indexSchool from "./src/school/route";
+import indexStudent from "./src/student/route";
 
+//Routes
 app.use("/", indexRouter);
+app.use("/classroom", classroomRouter);
+app.use("/school", indexSchool);
+app.use("/student", indexStudent);
 
 app.listen(3000, () => console.log("Listening port 3000"));
